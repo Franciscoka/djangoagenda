c80d54a074d3b5e30cc9f3fc5b43f319599fac64
Franciscoka <franciscoka@hotmail.com>
2023-07-01 13:14:56 -0300

    Criando o app contact no Django

--- project/settings.py
+++ project/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-$fof0^e74nb5)v_09*-gp*h_enb42r)wr(ggq*lq2o!2g5tngo"
+SECRET_KEY = "django-insecure-$fof0^e74nb5)v_09*-gp*h_enb42r)wr(ggq*lq2o!2g5tngo"   # noqa
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = []  # type:ignore
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "contact",
 ]
 
 MIDDLEWARE = [
